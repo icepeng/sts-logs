1c5f6afaa8a40758d3c395e44418a60a01729760
Ha <typ0@naver.com>
2018-03-16 18:00:24 +0900

    chart test

--- src/app/card/components/run-upgrade.component.ts
+++ src/app/card/components/run-upgrade.component.ts
@@ -1,16 +1,78 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+    OnChanges,
+    OnInit,
+} from '@angular/core';
+
+import { RunCard } from '../models/run-card.model';
 
 @Component({
-  selector: 'app-run-upgrade',
-  templateUrl: './run-upgrade.component.html',
-  styles: [],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-run-upgrade',
+    templateUrl: './run-upgrade.component.html',
+    styles: [
+        `
+    .chart {
+      height: 240px;
+    }`,
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RunUpgradeComponent implements OnInit {
+export class RunUpgradeComponent implements OnInit, OnChanges {
+    @Input() card: string;
+    @Input() runCard: RunCard[];
 
-  constructor() { }
+    options = {
+        tooltip: {},
+        dataset: {
+            source: [
+                {
+                    name: '1',
+                    value: 5,
+                },
+                {
+                    name: '2',
+                    value: 10,
+                },
+            ],
+        },
+        series: [
+            {
+                name: 'pie',
+                type: 'pie',
+            },
+        ],
+    };
 
-  ngOnInit() {
-  }
+    constructor() {}
 
+    ngOnInit() {}
+
+    ngOnChanges() {
+        this.options = {
+            ...this.options,
+            dataset: {
+                source: this.getRunsByUpgrade(this.card, this.runCard),
+            },
+        };
+    }
+
+    getRunsByUpgrade(
+        card: string,
+        runCard: RunCard[],
+    ): { name: string; value: number }[] {
+        return Object.entries(
+            runCard.filter(x => x.card === card).reduce(
+                (obj, x) => {
+                    const item = obj[x.maxUpgrade] || 0;
+                    return {
+                        ...obj,
+                        [x.maxUpgrade]: item + 1,
+                    };
+                },
+                {} as { [upgrade: number]: number },
+            ),
+        ).map(([name, value]) => ({ name, value }));
+    }
 }
--- src/app/card/components/winrate-upgrade.component.ts
+++ src/app/card/components/winrate-upgrade.component.ts
@@ -1,16 +1,90 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+    OnChanges,
+    OnInit,
+} from '@angular/core';
+
+import { Run } from '../../run/models/run.model';
+import { RunCard } from '../models/run-card.model';
 
 @Component({
-  selector: 'app-winrate-upgrade',
-  templateUrl: './winrate-upgrade.component.html',
-  styles: [],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-winrate-upgrade',
+    templateUrl: './winrate-upgrade.component.html',
+    styles: [
+        `
+  .chart {
+    height: 240px;
+  }`,
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WinrateUpgradeComponent implements OnInit {
+export class WinrateUpgradeComponent implements OnInit, OnChanges {
+    @Input() card: string;
+    @Input() runCard: RunCard[];
+    @Input() runEntities: { [id: string]: Run };
 
-  constructor() { }
+    options = {
+        tooltip: {},
+        dataset: {
+            source: [
+                {
+                    name: '1',
+                    value: 5,
+                },
+                {
+                    name: '2',
+                    value: 10,
+                },
+            ],
+        },
+        series: [
+            {
+                name: 'bar',
+                type: 'bar',
+            },
+        ],
+    };
 
-  ngOnInit() {
-  }
+    constructor() {}
 
+    ngOnInit() {}
+
+    ngOnChanges() {
+        this.options = {
+            ...this.options,
+            dataset: {
+                source: this.getWinrateByUpgrade(
+                    this.card,
+                    this.runCard,
+                    this.runEntities,
+                ),
+            },
+        };
+    }
+
+    getWinrateByUpgrade(
+        card: string,
+        runCard: RunCard[],
+        runEntities: { [id: string]: Run },
+    ): { name: string; value: number }[] {
+        return Object.entries(
+            runCard.filter(x => x.card === card).reduce(
+                (obj, x) => {
+                    const item = obj[x.maxUpgrade] || { run: 0, win: 0 };
+                    return {
+                        ...obj,
+                        [x.maxUpgrade]: {
+                            run: item.run + 1,
+                            win: runEntities[x.run].victory
+                                ? item.win + 1
+                                : item.win,
+                        },
+                    };
+                },
+                {} as { [upgrade: number]: { run: number; win: number } },
+            ),
+        ).map(([name, x]) => ({ name, value: x.win / x.run }));
+    }
 }
